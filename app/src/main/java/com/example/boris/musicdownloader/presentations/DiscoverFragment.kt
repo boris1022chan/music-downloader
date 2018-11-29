dfa30473cd8d04304c81e8b562e1794db3d5d74f
boris1022chan <boris1022chan@yahoo.com.hk>
2018-11-28 23:21:15 -0500

    add web view to discover tab

--- app/src/main/java/com/example/boris/musicdownloader/presentations/DiscoverFragment.kt
+++ app/src/main/java/com/example/boris/musicdownloader/presentations/DiscoverFragment.kt
@@ -5,13 +5,25 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import com.example.boris.musicdownloader.R
 
 
 class DiscoverFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_discover, container, false)
+    private val TAG = "DiscoverFragment"
 
+    private lateinit var discoverContentWebView: WebView
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val v = inflater.inflate(R.layout.fragment_discover, container, false)
+
+        discoverContentWebView = v.findViewById(R.id.discover_webview)
+        discoverContentWebView.apply {
+            settings.javaScriptEnabled = true
+            loadUrl("https://m.youtube.com/channel/UC-9-kyTW8ZkZNDHQJ6FgpwQ")
+        }
+
+        return v
     }
 }
