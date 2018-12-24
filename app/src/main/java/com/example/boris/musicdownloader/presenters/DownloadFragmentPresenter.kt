d8fc787a57b009e1d04629f9697c649b8fce64ce
boris1022chan <boris1022chan@yahoo.com.hk>
2018-12-23 23:18:01 -0500

    refactor into clean architecture & rxjava

--- app/src/main/java/com/example/boris/musicdownloader/presenters/DownloadFragmentPresenter.kt
+++ app/src/main/java/com/example/boris/musicdownloader/presenters/DownloadFragmentPresenter.kt
@@ -1,5 +1,6 @@
 package com.example.boris.musicdownloader.presenters
 
+import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
@@ -8,6 +9,7 @@ import android.os.Environment
 import android.os.Environment.DIRECTORY_MUSIC
 import android.util.Log
 import com.example.boris.musicdownloader.presentations.DownloadFragment
+import com.example.boris.musicdownloader.usecases.YoutubeAudioDownloadUseCaseImpl
 import com.example.boris.musicdownloader.utils.YoutubeUtil
 import com.github.axet.vget.VGet
 import com.github.axet.vget.info.VideoFileInfo
@@ -21,6 +23,7 @@ import com.github.hiteshsondhi88.libffmpeg.FFmpeg
 import com.github.hiteshsondhi88.libffmpeg.LoadBinaryResponseHandler
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException
 import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException
+import io.reactivex.schedulers.Schedulers
 import java.io.File
 import java.net.URL
 import java.util.concurrent.atomic.AtomicBoolean
@@ -35,6 +38,8 @@ class DownloadFragmentPresenterImpl(val view: DownloadFragment) : DownloadFragme
 
     // TODO: fix tag
     private val TAG = "DiscoverFragmentPresent"
+
+    private val usecase by lazy { YoutubeAudioDownloadUseCaseImpl() }
 
     override fun downloadButtonAction(input: String) {
         val link = input.trim()
@@ -51,13 +56,18 @@ class DownloadFragmentPresenterImpl(val view: DownloadFragment) : DownloadFragme
         return "http://www.youtube.com/watch?v=$token"
     }
 
+    @SuppressLint("CheckResult")
     private fun downloadMusic(link: String) {
         if (!view.haveWritePermission()) {
             view.requestPermission()
             return
         }
 
-        DownloadMusicTask(view.context!!).execute(URL(link))
+        usecase.execute(URL(link), view.context!!)
+            .subscribeOn(Schedulers.io())
+            .subscribe({}, { e ->
+                e.printStackTrace()
+            })
     }
 
     private fun externalStorageAvail(): Boolean {
@@ -66,233 +76,5 @@ class DownloadFragmentPresenterImpl(val view: DownloadFragment) : DownloadFragme
 
     companion object {
         val pattern: Pattern by lazy { Pattern.compile("^http(s?)://(?:www\\.)?youtu(?:be\\.com/watch\\?v=|\\.be/)([\\w\\-_]*)(.*)") }
-    }
-}
-
-
-private class DownloadMusicTask(
-    private val mContext: Context
-) : AsyncTask<URL, Unit, Unit>() {
-
-    override fun doInBackground(vararg params: URL?) {
-        if (params[0] == null) {
-            Log.d("Boris", "link is not received")
-            return
-        }
-
-        Log.d("Boris", "start download")
-        val linkUrl = params[0]
-        val path = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MUSIC)
-
-        try {
-            val user = VGet.parser(linkUrl)
-            val videoInfo = user.info(linkUrl)
-            val v = VGet(videoInfo, path)
-            val notify = VGetStatus(videoInfo)
-            val stop: AtomicBoolean = AtomicBoolean(false)
-
-            v.extract(user, stop, notify)
-            val title = videoInfo.title.replace("/", "")
-
-            System.out.println("Title: $title")
-            val list = videoInfo.info
-            if (list != null) {
-
-                for (d in list) {
-                    println("Download URL: " + d.source)
-                }
-            }
-
-            v.download(user, stop, notify)
-
-            val mp4File = File(videoInfo.info.get(0).targetFile.toString())
-            if (mp4File.exists()) {
-                Log.d("Boris", "Boris downloade complete delete file")
-                mp4File.delete()
-            } else {
-                Log.d("Boris", "Boris downloade complete not found ")
-            }
-
-            val ffmpeg = FFmpeg.getInstance(mContext)
-            try {
-                ffmpeg.loadBinary(object : LoadBinaryResponseHandler() {
-
-                    override fun onStart() {}
-
-                    override fun onFailure() {}
-
-                    override fun onSuccess() {}
-
-                    override fun onFinish() {}
-                })
-            } catch (e: FFmpegNotSupportedException) {
-                // Handle if FFmpeg is not supported by device
-            }
-            try {
-                ffmpeg.execute(
-                    arrayOf(
-                        "-i",
-                        videoInfo.info.get(1).targetFile.toString(),
-                        "${Environment.getExternalStoragePublicDirectory(DIRECTORY_MUSIC)}/$title.mp3"
-                    ), object : ExecuteBinaryResponseHandler() {
-
-                        override fun onStart() {}
-
-                        override fun onProgress(message: String?) {
-                            Log.d("ffmpegevent", "onProgress: " + message!!)
-                        }
-
-                        override fun onFailure(message: String?) {
-                            Log.d("ffmpegevent", "onProgress: " + message!!)
-                            Log.d("ffmpegevent", "onFailure: ")
-                        }
-
-                        override fun onSuccess(message: String?) {
-                            Log.d("ffmpegevent", "onSuccess: ")
-
-                        }
-
-                        override fun onFinish() {
-                            Log.d("ffmpegevent", "onProgress: Finished")
-
-                            val webmFile = File(videoInfo.info.get(1).targetFile.toString())
-                            if (webmFile.exists()) {
-                                Log.d("Boris", "Boris convert complete delete file")
-                                webmFile.delete()
-                            } else {
-                                Log.d("Boris", "Boris convert complete not found ")
-                            }
-
-                            val mp3File = File("${Environment.getExternalStoragePublicDirectory(DIRECTORY_MUSIC)}/$title.mp3")
-                            mContext.sendBroadcast(Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE, Uri.fromFile(mp3File)))
-                        }
-                    })
-            } catch (e: FFmpegCommandAlreadyRunningException) {
-                // Handle if FFmpeg is already running
-                e.printStackTrace()
-            }
-
-        } catch (e: Exception) {
-            e.printStackTrace()
-            throw e
-        }
-    }
-}
-
-internal class VGetStatus(var videoinfo: VideoInfo) : Runnable {
-    var last: Long = 0
-
-    var map: MutableMap<VideoFileInfo, SpeedInfo> = HashMap()
-
-    fun getSpeedInfo(dinfo: VideoFileInfo): SpeedInfo {
-        var speedInfo = map[dinfo]
-        if (speedInfo == null) {
-            speedInfo = SpeedInfo()
-            speedInfo!!.start(dinfo.count)
-            map[dinfo] = speedInfo!!
-        }
-        return speedInfo
-    }
-
-    override fun run() {
-        val dinfoList = videoinfo.info
-
-        // notify app or save download state
-        // you can extract information from DownloadInfo info;
-        when (videoinfo.state) {
-            VideoInfo.States.EXTRACTING, VideoInfo.States.EXTRACTING_DONE, VideoInfo.States.DONE -> {
-                if (videoinfo is YouTubeInfo) {
-                    val i = videoinfo as YouTubeInfo
-                    println((videoinfo.getState()).toString() + " " + i.videoQuality)
-                } else if (videoinfo is VimeoInfo) {
-                    val i = videoinfo as VimeoInfo
-                    println((videoinfo.getState()).toString() + " " + i.videoQuality)
-                } else {
-                    println("downloading unknown quality")
-                }
-                for (d in videoinfo.info) {
-                    val speedInfo = getSpeedInfo(d)
-                    speedInfo.end(d.count)
-                    println(
-                        String.format(
-                            "file:%d - %s (%s)", dinfoList!!.indexOf(d), d.targetFile,
-                            formatSpeed(speedInfo.averageSpeed.toLong())
-                        )
-                    )
-                }
-            }
-            VideoInfo.States.ERROR -> {
-                println((videoinfo.getState()).toString() + " " + videoinfo.delay)
-
-                if (dinfoList != null) {
-                    for (dinfo in dinfoList!!) {
-                        println(
-                            "file:" + dinfoList!!.indexOf(dinfo) + " - " + dinfo.exception + " delay:"
-                                    + dinfo.delay
-                        )
-                    }
-                }
-            }
-            VideoInfo.States.RETRYING -> {
-                println((videoinfo.getState()).toString() + " " + videoinfo.delay)
-
-                if (dinfoList != null) {
-                    for (dinfo in dinfoList!!) {
-                        println(
-                            ("file:" + dinfoList!!.indexOf(dinfo) + " - " + dinfo.state + " "
-                                    + dinfo.exception + " delay:" + dinfo.delay)
-                        )
-                    }
-                }
-            }
-            VideoInfo.States.DOWNLOADING -> {
-                val now = System.currentTimeMillis()
-                if (now - 1000 > last) {
-                    last = now
-
-                    var parts = ""
-
-                    for (dinfo in dinfoList!!) {
-                        val speedInfo = getSpeedInfo(dinfo)
-                        speedInfo.step(dinfo.count)
-
-                        val pp = dinfo.parts
-                        if (pp != null) {
-                            // multipart download
-                            for (p in pp!!) {
-                                if (p.state.equals(DownloadInfo.Part.States.DOWNLOADING)) {
-                                    parts += String.format(
-                                        "part#%d(%.2f) ", p.number,
-                                        p.count / p.length.toFloat()
-                                    )
-                                }
-                            }
-                        }
-                        println(
-                            String.format(
-                                "file:%d - %s %.2f %s (%s)", dinfoList!!.indexOf(dinfo),
-                                videoinfo.state, dinfo.count / dinfo.length.toFloat(), parts,
-                                formatSpeed(speedInfo.currentSpeed.toLong())
-                            )
-                        )
-                    }
-                }
-            }
-            else -> {
-            }
-        }
-    }
-}
-
-fun formatSpeed(s: Long): String {
-    if (s > 0.1 * 1024.0 * 1024.0 * 1024.0) {
-        val f = s.toFloat() / 1024f / 1024f / 1024f
-        return String.format("%.1f GB/s", f)
-    } else if (s > 0.1 * 1024.0 * 1024.0) {
-        val f = s.toFloat() / 1024f / 1024f
-        return String.format("%.1f MB/s", f)
-    } else {
-        val f = s / 1024f
-        return String.format("%.1f kb/s", f)
     }
 }
